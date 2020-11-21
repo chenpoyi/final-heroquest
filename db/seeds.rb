f6fe0f1a311a463da71ca5fe418bbc60cde84246
Scott Morton <smorton@Scotts-MacBook-Pro.local>
2020-11-20 17:08:18 -0700

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Helper functions
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
+
+# Only run on development (local) instances not on production, etc.
+unless Rails.env.development?
+  puts "Development seeds only (for now)!"
+  exit 0
+end
+
+## Hero
+
+puts "Re-creating Heros ..."
+
+Hero.destroy_all
+
+heroes = Hero.create({
+  race: barbarian, 
+  attack: 3, 
+  defend: 2, 
+  body: 8 , 
+  mind: 2, 
+  image: open_asset('Barbarian-small.gif')
+  })
+
+heroes = Hero.create({
+  race: dwarf, 
+  attack: 2, 
+  defend: 2, 
+  body: 7, 
+  mind: 3, 
+  image: open_asset('Dwarf-small.gif')
+  })
+
+heroes = Hero.create({
+  race: elf, 
+  attack: 2, 
+  defend: 2, 
+  body: 6, 
+  mind: 4, 
+  image: open_asset('Elf-small.gif')})
+  
+heroes = Hero.create({
+  race: wizard, 
+  attack: 1, 
+  defend: 2, 
+  body: 4, 
+  mind: 6, 
+  image: open_asset('wizard-small.gif')})
