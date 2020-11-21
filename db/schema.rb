bff9b147dfe64a00ce30364c9a97ca00ab908d01
Scott Morton <smorton@Scotts-MacBook-Pro.local>
2020-11-20 18:28:36 -0700

    updating db with models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_012225) do
+ActiveRecord::Schema.define(version: 2020_11_21_012504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 2020_11_21_012225) do
     t.string "description"
     t.boolean "chaos"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
