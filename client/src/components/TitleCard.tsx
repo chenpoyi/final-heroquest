25631c813df8016bd18aca72cae092f33b33141e
Scott Morton <smorton@Scotts-MacBook-Pro.local>
2020-11-23 11:25:18 -0700

    adding grid container for title card

--- client/src/components/TitleCard.tsx
+++ client/src/components/TitleCard.tsx
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
   root: {
@@ -22,32 +23,34 @@ export default function TitleCard() {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://i.imgur.com/tmhlFew.jpg"
-          title="HeroQuest Title"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            HeroQuest
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          A companion application for the 1990's classic board game. 
+    <Grid container>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://i.imgur.com/tmhlFew.jpg"
+            title="HeroQuest Title"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              HeroQuest
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+            A companion application for the 1990's classic board game. 
 
-Deep inside another dimension, face battling barbarians and evil magic on a quest for adventure in a maze of monsters. This is HeroQuest, the fantasy adventure game, where winning means mastering the art of combat and magic. Once you get into it, you'll never be the same
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button variant="contained" size="small" color="primary">
-          Login
-        </Button>
-        <Button variant="contained" size="small" color="primary">
-          Sign Up
-        </Button>
-      </CardActions>
-    </Card>
+  Deep inside another dimension, face battling barbarians and evil magic on a quest for adventure in a maze of monsters. This is HeroQuest, the fantasy adventure game, where winning means mastering the art of combat and magic. Once you get into it, you'll never be the same
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button variant="contained" size="small" color="primary">
+            Login
+          </Button>
+          <Button variant="contained" size="small" color="primary">
+            Sign Up
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
